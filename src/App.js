21e7cfd9398328361032aca4b9b66a799b6df3fc
kimdlzp <kimdlzp@gmail.com>
2022-01-24 19:05:31 +0900

    main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import List from './components/answer/List';
+import SignIn from './components/user/SignIn';
 
 function App() {
-  return <List />;
+  return <SignIn />;
 }
 
 export default App;
--- src/components/user/SignIn.js
+++ src/components/user/SignIn.js
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/main.scss';
 
-const SignIn = () => {};
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const onClick = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <div className="logo">Logo Image</div>
+      </div>
+      <div className="signin-container">
+        <div className="email-container">
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={onChangeEmail}
+            required
+          ></input>
+        </div>
+        <div className="password-container">
+          <input
+            type="password"
+            name="pasword"
+            placeholder="password"
+            value={password}
+            onChange={onChangePassword}
+            required
+          ></input>
+        </div>
+        <div className="signin-btn-container">
+          <button onClick={onClick}>signin</button>
+        </div>
+        <hr></hr>
+      </div>
+      <div className="social-container">
+        <span className="social-icon">소셜 아이콘</span>
+        <span className="move-social">소셜페이지로</span>
+      </div>
+
+      <div className="signup-info-container">
+        <span className="signup-info">계정이 없으신가요?</span>
+        <span className="move-signups">가입하기</span>
+      </div>
+    </div>
+  );
+};
 
 export default SignIn;
